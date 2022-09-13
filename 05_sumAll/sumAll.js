f23378bf7229132ab4a32d60de53201bd9ad8ef5
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2022-09-13 11:22:38 +0200

    05 finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (int1, int2) {
+    //check if not number & if negativ
+    if (typeof int1 != 'number' ||typeof int2 != 'number' || int1 < 0 || int2 < 0) return "ERROR";
+    //get numbers between int1 & int2 into array
+    const numbersBetween = [];
+    let sum = 0;
+    //add all numbers togehter in loop
+    if (int1 > int2) {
+        for (let i = int2; i < int1 + 1; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (let i = int1; i < int2 + 1; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
