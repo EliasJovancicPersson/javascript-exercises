8047dadf8447f58aa37ce57bccd395cc884ccb58
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2022-10-07 09:28:39 +0200

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(termA, termB) {
+  return termA + termB;
 };
 
-const subtract = function() {
-	
+const subtract = function(termA, termB) {
+  return termA - termB;
 };
 
-const sum = function() {
-	
+const sum = function(terms) {
+  return terms.reduce((a, b) => a + b,0);
 };
 
-const multiply = function() {
-
+const multiply = function(terms) {
+  return terms.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(number,exp) {
+  let total = number;
+  for (let i = 0; i < (exp -1); i++) {
+    total = total * number;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(fac) {
+  let total = 1;
+  for (let i = fac; i > 1; i--) {
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
