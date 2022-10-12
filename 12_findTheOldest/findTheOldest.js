cb3016272cce0c6b16fa4b68b2aa462ae9516e11
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2022-10-12 17:02:43 +0200

    oldest finsihed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (obj) {
+     const getAge = function (birth, death) {
+        if (!death) {
+            death = new Date().getFullYear();
+            console.log("death set to " + death + " for person born "+ birth)
+        }
+        return death - birth;
+    }
+    
+    return oldest = obj.reduce((oldest, curr) => {
+        const oldestAge = getAge(oldest.yearOfBirth,oldest.yearOfDeath)
+        const currAge = getAge(curr.yearOfBirth,curr.yearOfDeath)
+        return oldestAge < currAge ? curr : oldest
+    })
 };
 
 // Do not edit below this line
